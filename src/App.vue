ba24d6ace6eba6b697a16915ac9349e23569af31
RiadArefin <arefinriad40@gmail.com>
2022-09-20 14:32:50 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+
+
+<template>
+   <div class="navbar d-flex text-white bg-dark ">
+      <h3 class="p-4 mx-3">Dashboard</h3>
+      <div class="d-flex ">
+         <i class="fa-solid fa-user-tie fs-1"></i>
+         <h3 class=" mx-2">Demo User</h3>
+     
+      </div>
+    
+     
+   </div>
+   <User :items="collapsibleItems"/>
+   
+</template>
+
+<script>
+ 
+  import User from './components/Dashboard.vue'
+  import {ref}from'vue'
+   export default{
+    name:"App",
+
+     setup(){
+       const collapsibleItems= ref([   { id:1,
+           header:'User',
+           body:'<div class="text-white"><h1 class="text-center">User</h1><p>Where can I get some?There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p></div>',
+          upper:true,
+            icon:true,
+           active:false,
+           toggle:true,
+         },
+           {id:2,
+           header:'View-Content',
+            body:'<div class="text-white"> <div class="  p-2"> <u><h3 class="text-info">View Content</h3></u> </div> <h1 class="text-center">View</h1> <b>What is lorem ipsam?</b>  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> <b>Why do we use it?</b> <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</div></p>',
+            upper:false,
+           icon:false,
+           active:true,
+            toggle:true,
+         },]);
+
+      return{
+         collapsibleItems:collapsibleItems
+      }
+     },
+     components:{
+      User,
+      
+     }
+    //--------------------Options Api-------------------
+   //   data(){
+   //   return{
+   //     collapsibleItems:[
+   //        { id:1,
+   //         header:'User',
+   //         body:'<div class="text-white">Where can I get some?There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>',
+   //        upper:true,
+   //          icon:true,
+   //         active:false,
+   //         toggle:true,
+   //     },
+   //         {id:2,
+   //         header:'View-Content',
+   //          body:'<div class="text-white"> <div class="  p-2"> <u><h3 class="text-info">View Content</h3></u> </div> <h1 class="text-center">View</h1> <b>What is lorem ipsam?</b>  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> <b>Why do we use it?</b> <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.</div></p>',
+   //          upper:false,
+   //          icon:false,
+   //          active:true,
+   //          toggle:true,
+   //        },
+   //     ]
+   //   }
+   //   },
+   //  components:{
+   //   User,
+   //  }
+
+   }
+
+</script>
+
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,148 @@
+<template>
+<div class="dashboard d-flex flex-row-reverse justify-content-between ">
+    <div class="collapse-wrapper ">
+    
+    <div  v-for="(item,index) in customItems" :key="item.header"  class="collapsible-item mx-5">
+        <Transition name="toggle">
+             <div v-if="!item.toggle"  @click="reverse(index)"    class="collapsible-header d-flex justify-content-between  ">
+               <h4> {{item.header}}</h4>
+               <i v-if="item.active" @click.stop="activateBody(index,event)" class="fa-solid fa-xmark cursor fs-2 "></i>
+               <i v-if="!item.active" @click.stop="activateBody(index,event)" class="fa-solid fa-sort-down cursor fs-2 "></i>
+           </div>
+         </Transition>
+        <Transition name="collapse">
+            <div v-html="item.body" v-if="item.active" class="  collapsible-body">
+           
+           </div>
+        </Transition>
+    </div> 
+    </div> 
+      <div class="menu-bar d-flex p-5">
+           <i class="fa-solid fa-house text-white fs-1"></i>
+          <h1   @mouseover="hover()" @mouseleave="leave()"
+                 class="text-white ">Home</h1>
+                
+     </div>
+  </div>   
+</template>
+<script>
+//import{computed,ref,toRefs} from 'vue'
+export default {
+    name:'Dashboard',
+    props:[
+      "items"
+    ],
+
+    //  setup(props){
+    //    const collapsibleItems= ref([]);
+    //    const { items } = toRefs(props)
+    //     const customItems=computed(function(){
+    //        collapsibleItems=items.map((item)=>{
+    //          return{
+    //              header:item.header,
+    //                body:item.body,
+    //                active:item.active,
+    //               icon:item.icon
+    //           }
+    //         });
+    //         return collapsibleItems
+    //   });
+ 
+
+    //  return{
+    //   customItems
+    //  }
+    //  },
+   
+    data(){
+        return{
+              collapsibleItems:[]
+         }
+     },
+      computed:{
+         customItems(){
+             this.collapsibleItems=this.items.map((item)=>{
+                   return{
+                 header:item.header,
+                 body:item.body,
+                  active:item.active,
+                  icon:item.icon
+             }
+             }
+             );
+             return this.collapsibleItems;
+         }
+      },
+
+
+    methods:{
+        activateBody(index,event){
+            console.log(index);
+            this.collapsibleItems[index].active = !this.collapsibleItems[index].active;
+            this.collapsibleItems.forEach((item,id)=>{
+                if(id!==index){
+                 item.active=false;
+                }
+            })
+        
+        },
+     
+        reverse(index){
+            console.log('reverse');
+           
+            this.collapsibleItems[index].toggle= !this.collapsibleItems[index].toggle;
+          
+           setTimeout(()=>{ 
+               this.collapsibleItems[1].toggle= false;
+                this.collapsibleItems[0].toggle= false;
+                 this.collapsibleItems= this.collapsibleItems.reverse();
+               console.log('sett')
+          }, 1000);
+        },
+
+        hover(){
+          console.log('hover');
+            this.collapsibleItems[1].active = !this.collapsibleItems[1].active;
+           
+        },
+        leave(){
+          console.log('leave');
+            this.collapsibleItems[1].active = !this.collapsibleItems[1].active;
+        }
+
+    }
+}
+</script>
+<style scoped>
+  
+  
+               /* code for animation with Vue Transition  */
+ 
+ 
+  .collapse-enter-active{
+   height: 30px;  
+  }
+  .collapse-enter-to{
+    height: 500px;
+  }
+  .collapse-leave-to{
+    height: 30px;   
+  }
+    .cursor{
+    cursor: pointer;
+  }
+  .toggle-enter-from{
+  transition: all 3s linear;
+  }
+  .toggle-enter-to{
+   transition: all 3s linear;
+  }
+.toggle-leave-active{
+     transition: all 2s cubic-bezier(1, 0.8, 0.8, 1);
+
+}
+  .toggle-leave-to{
+     transform: translatey(100px);
+    opacity: 0;
+  }
+</style>
